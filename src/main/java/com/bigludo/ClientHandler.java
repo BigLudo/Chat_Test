66d6dee1ad4070b9ff896a99c141af59e008815c
BigLudo <ludwigahnquist@gmail.com>
2023-11-09 08:46:55 +0100

    added method to enable listening in clienthandler

--- src/main/java/com/bigludo/ClientHandler.java
+++ src/main/java/com/bigludo/ClientHandler.java
@@ -1,22 +1,30 @@
 package com.bigludo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ClientHandler implements IClient{
-
+public class ClientHandler implements IClient, Runnable{
     private static final Logger log = LogManager.getLogger(ClientHandler.class);
-
     private String ID;
     private Socket clientSocket;
-    private IServer server;    
+    private IServer server;
+    private BufferedReader br;
 
     public ClientHandler (String id, Socket cs, IServer srv) {
         ID = id;
         clientSocket = cs;
         server = srv;
         log.info("ClientHandler created " + id);
+
+        try {
+            br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            log.error("Error init BufferedReader: " + e.getMessage());
+        }
     }
 
     public void sendMessage(String message) {
@@ -31,5 +39,27 @@ public class ClientHandler implements IClient{
     @Override
     public String getId() {
         return ID;
+    }
+
+    public void listenToMessages() {
+        try {
+            while (true) {
+                String message = br.readLine();
+
+                if (message == null) {
+                    log.info("Client disconnected: " + ID);
+                    break;
+                }
+
+                recieveMessage(message);
+            }
+        } catch (IOException e) {
+            log.error("Error intercepting message: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        listenToMessages();
     }
 }
